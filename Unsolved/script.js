867b64f816a3eade3dd8cb466a681ce90c69dcba
Zin KO <zinko.it@outlook.com>
2024-08-31 23:52:06 +0630

    Added some local scope and changes to show message in console

--- Unsolved/script.js
+++ Unsolved/script.js
@@ -1,11 +1,12 @@
 // TODO: Declare variable 'shout' with the value 'Shout' so it's available to `justShout` and `shoutItAllOut` functions
+let shout = "Shout";
 function justShout() {
-  console.log(shout + ', ' + shout);
+  console.log(shout + ", " + shout);
   return;
 }
 
 function shoutItAllOut() {
-  console.log(shout + ' it all out! ');
+  console.log(shout + " it all out! ");
   return;
 }
 
@@ -14,22 +15,23 @@ shoutItAllOut();
 
 // TODO: Declare variable 'animal' with the value 'Tigers' so it is only available to the 'sayTigers' function
 function sayLions() {
-  var animal = 'Lions';
+  var animal = "Lions";
   console.log(animal);
   return;
 }
 
 function sayTigers() {
-  console.log('and ' + animal + ' and ');
+  let animal = "Tigers";
+  console.log("and " + animal + " and ");
   return;
 }
 
 // TODO: The variable 'bears' should only declared once and 'sayBears' should return "Bears! OH MY!".
-var bears = 'Bears';
+var bears = "Bears";
 
 function sayBears() {
-  var bears = 'Pandas';
-  console.log(bears + '! OH MY!');
+  // var bears = 'Pandas';
+  console.log(bears + "! OH MY!");
   return;
 }
 
@@ -38,12 +40,12 @@ sayTigers();
 sayBears();
 
 // TODO: The variable 'sing' should be declared once in the local scope.
-var sing = 'Sing';
 
 function singAlong() {
-  console.log(sing + ',');
-  var singASong = function() {
-    console.log(sing + ' a Song.');
+  var sing = "Sing";
+  console.log(sing + ",");
+  var singASong = function () {
+    console.log(sing + " a Song.");
   };
   singASong();
 }
